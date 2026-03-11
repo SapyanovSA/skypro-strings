239d77b0f6b4679c25dd3b8c35b595ceca2ffa39
helloworld(1) <sapanovs04@gmail.com>
2026-03-11 18:44:17 +0300

    Homework-10

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Task 1
+        System.out.println("Task 1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //Task 2
+        System.out.println("Task 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //Task 3
+        System.out.println("Task 3");
+
+        fullName = "Иванов Семён Семёнович";
+
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
